3d6c236b81785fc09bbabed24a1579153a088dd7
serafm <cs04555@uoi.gr>
2023-08-30 14:04:32 +0300

    [ADDED] Added docstrings in the functions

--- odoo_login.py
+++ odoo_login.py
@@ -4,6 +4,7 @@ import odoorpc
 class OdooLogin:
 
     def __init__(self, host, db_name, user, password, odoo=None):
+        """ Initialize user credentials """
         self.db_name = db_name
         self.user = user
         self.password = password
@@ -11,22 +12,26 @@ class OdooLogin:
         self.host = host
 
     def connect_server(self):
+        """ Connect to the Odoo server """
         self.odoo = odoorpc.ODOO(self.host, port=8016)
 
     def database_login(self):
+        """ Login in to the database """
         self.odoo.login(self.db_name, self.user, self.password)
 
 
 class FetchData:
 
-    def __init__(self, data_model_name, records=None, odoo=None):
-        self.data_model_name = data_model_name
+    def __init__(self, model_name, records=None, odoo=None):
+        """ Initialize model name for data retrieval """
+        self.model_name = model_name
         self.odoo = odoo
         self.records = records
 
     def fetch_data(self):
-        if self.data_model_name in self.odoo.env:
-            self.records = self.odoo.env[self.data_model_name]
+        """ Retrieve data from the database and print the results """
+        if self.model_name in self.odoo.env:
+            self.records = self.odoo.env[self.model_name]
             product_ids = self.records.search([])
             for product in self.records.browse(product_ids):
                 print(product.name)
