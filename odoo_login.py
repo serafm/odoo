d26707e962eacc3843bfc7e91264b10451017a9a
serafm <cs04555@uoi.gr>
2023-08-30 15:49:02 +0300

    [UPDATE] Added try catch in connect_server() function

--- odoo_login.py
+++ odoo_login.py
@@ -5,40 +5,38 @@ class OdooLogin:
 
     def __init__(self, host, db_name, user, password, odoo=None):
         """ Initialize user credentials """
+        self.host = host
         self.db_name = db_name
         self.user = user
         self.password = password
         self.odoo = odoo
-        self.host = host
 
     def connect_server(self):
         """ Connect to the Odoo server """
-        self.odoo = odoorpc.ODOO(self.host, port=8016)
+        try:
+            self.odoo = odoorpc.ODOO(self.host, port=8069)
+        except Exception as e:
+            # Handle the exception
+            print(f"Error connecting to the server: {str(e)}")
 
     def database_login(self):
-        """ Login in to the database """
+        """ Login in to the database with user credentials"""
         self.odoo.login(self.db_name, self.user, self.password)
 
 
 class FetchData:
 
-    def __init__(self, model_name, records=None, odoo=None):
+    def __init__(self, model_name, data=None, odoo=None):
         """ Initialize model name for data retrieval """
         self.model_name = model_name
         self.odoo = odoo
-        self.records = records
+        self.data = data
 
     def fetch_data(self):
-        """ Retrieve data from the database and print the results """
+        """ Retrieve data from the database from the given model name and print the results """
         if self.model_name in self.odoo.env:
-            self.records = self.odoo.env[self.model_name]
-            product_ids = self.records.search([])
-            for product in self.records.browse(product_ids):
-                print(product.name)
-                print(product.price)
+            self.data = self.odoo.execute(self.model_name, 'read', [], ['name', 'price'])
 
-
-# HERE EN KSERO TI KAMNO RE ZARTILIE
 
 odoo_login = OdooLogin(
     'localhost',
@@ -50,3 +48,7 @@ odoo_login = OdooLogin(
 odoo_login.connect_server()
 
 odoo_login.database_login()
+
+records = FetchData('product.product')
+
+records.fetch_data()
