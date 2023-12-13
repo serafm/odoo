18c5a556db7a73d6431db58b247515ac9f24c397
serafm <cs04555@uoi.gr>
2023-12-13 18:07:08 +0200

    [UPDATE] Main definition.

--- main/src/main.py
+++ main/src/main.py
@@ -3,24 +3,21 @@ from fetchData import FetchData
 from server import ServerDialog
 
 
-# Server object. Replace host and port with valid ones.
-server = ServerDialog('localhost', 5432)
+def main():
+    # Server object. Replace host and port with valid ones.
+    server = ServerDialog('localhost', 5432)
+    # Connect to the server
+    odoo_login = server.connect_server()
 
-# Connect to the server
-odoo_login = server.connect_server()
+    # Database object with user credentials for login. Replace them with valid names.
+    database = DatabaseLogin('db_name', 'user', 'password', odoo_login)
+    # Login to database
+    database.connect_database()
 
-# Database object with user credentials for login. Replace them with valid names.
-database = DatabaseLogin('db_name',
-                         'user',
-                         'password',
-                         odoo_login)
+    # Fetch Data object to retrieve data from model name.
+    fetch = FetchData('product.product', odoo_login)
+    # Fetch data and print results
+    fetch.fetch_data()
 
-# Login to database
-database.connect_database()
 
-# Fetch Data object to retrieve data from model name.
-fetch = FetchData('product.product',
-                  odoo_login)
-
-# Fetch data and print results
-fetch.fetch_data()
+main()
